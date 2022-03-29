0f03c1005a2e3b2d8bd3804e34522a946dd4f739
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-29 15:30:32 -0400

    Adding setup.py

---
+++ openweatherclass/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OpenWeatherClass",
+    version="0.10.0",
+    author="Brian Hudson",
+    description="A Python implementation of OpenWeather's API",
+    url="https://github.com/bearhudson/OpenWeatherClass",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "openweatherclass"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
